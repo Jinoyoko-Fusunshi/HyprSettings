13057d5d05189ba1cb9b2dd02c2719e2136f91d8
Jinoyoko <jinoyoko@outlook.com>
2025-10-05 17:11:28 +0200

    Improved script files to use required environment variables from the sourced .env file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# meta information for the linux arch package build process
-export APPLICATION_NAME="hyprsettings"
-export APPLICATION_VERSION="0.2.0"
-export PACKAGE_VERSION="1"
-export APPLICATION_ARCH="x86_64"
-export BUILDDIR="../out"
-export PKGDEST="../out"
-export PACKAGER="Jinoyoko <jinoyoko@outlook.com>"
+# load application information
+source "scripts/.env"
 
 # build hyprsettings application
+if [ -d out ]; then
+  rm -r out
+fi
+
 mkdir -p out
 ( cd scripts && makepkg -fs )
 
